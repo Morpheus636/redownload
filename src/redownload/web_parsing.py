e686725c76cfdb74928ea690e17b874ac56e4359
Josh Levin <floydaroo@gmail.com>
2021-11-15 19:42:10 -0500

    (#1) Created web_parsing.py with download_from_url() function

---
+++ src/redownload/web_parsing.py
@@ -0,0 +1,16 @@
+""" The module for HTML parsing related functions. """
+import urllib.request
+
+import bs4
+
+
+def download_from_url(url: str) -> bs4.BeautifulSoup:
+    """Downloads an HTML page from a url and converts it to a BeautifulSoup object.
+
+    :param url: The URL to download HTML from, in a string.
+    :return: BeautifulSoup object extracted from the url.
+    """
+    request = urllib.request.urlopen(url)
+    html = request.read()
+    soup = bs4.BeautifulSoup(html)
+    return soup
