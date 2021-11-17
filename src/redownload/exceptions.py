569f9c876277e47afe7100800f6223e5b32bc181
Josh Levin <floydaroo@gmail.com>
2021-11-17 13:31:13 -0500

    (#2) Added docstrings in exceptions module.

--- src/redownload/exceptions.py
+++ src/redownload/exceptions.py
@@ -1,10 +1,18 @@
 class RedownloadException(Exception):
+    """The base exception for redownload. All other redownload exceptions are subclasses."""
+
     pass
 
 
 class WebParsingException(RedownloadException):
+    """The base exception for the web_parsing module. All exceptions raised by the web_parsing module are
+    subclasses.
+    """
+
     pass
 
 
 class NoLinksFoundInPage(WebParsingException):
+    """Exception raised by web_parsing.extract_links when no links are found in the provided BeautifulSoup object."""
+
     pass
