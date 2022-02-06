560d0d9cf1228c296fbbd9f9babd252d6eadee70
Josh Levin <floydaroo@gmail.com>
2022-02-06 01:33:38 -0500

    (#5) updates to __init__.py

--- src/redownload/__init__.py
+++ src/redownload/__init__.py
@@ -1,1 +1,7 @@
 from . import downloads, exceptions, web_parsing
+
+
+def redownload(archive_org_link, filetypes, output_dir):
+    page = web_parsing.html_from_url(archive_org_link)
+    links = web_parsing.extract_links(page, True, filetypes)
+    downloads.download_from_set(links, output_dir)
